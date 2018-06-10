b63813d3a957620e4aba0c54242ca3160bb411c2
DESKTOP-NH1GAIE\Dawid <dawidantczak1997@wp.pl>
2018-06-10 04:04:52 +0200

    no message

---
+++ app/src/main/java/com/example/dawid/tvgallery/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dawid.tvgallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
