6f004281bb2f210177b5ab972923622e6d1542e3
DESKTOP-NH1GAIE\Dawid <dawidantczak1997@wp.pl>
2018-06-10 22:45:34 +0200

    no message

--- app/src/main/java/com/example/dawid/tvgallery/MainFragment.java
+++ app/src/main/java/com/example/dawid/tvgallery/MainFragment.java
@@ -1,11 +1,8 @@
 package com.example.dawid.tvgallery;
 
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v17.leanback.app.BackgroundManager;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
@@ -26,30 +23,44 @@ import com.bumptech.glide.request.target.SimpleTarget;
 
 public class MainFragment extends BrowseFragment
 {
-    private DisplayMetrics mMetrics;
-    private BackgroundManager mBackgroundManager;
+    private DisplayMetrics metrics;
+    private BackgroundManager backgroundManager;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
 
-        prepareBackgroundManager();
-
+        setupBackgroundManagerAndMetrics();
         setupUIElements();
-
-        loadRows();
-
+        setupHeaderPictures();
         setupEventListeners();
     }
 
-    @Override
-    public void onDestroy()
+    private void setupBackgroundManagerAndMetrics()
     {
-        super.onDestroy();
+        backgroundManager = BackgroundManager.getInstance(getActivity());
+        backgroundManager.attach(getActivity().getWindow());
+        metrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
     }
 
-    private void loadRows()
+    private void setupUIElements()
+    {
+        setHeadersState(HEADERS_ENABLED);
+        setHeadersTransitionOnBackEnabled(true);
+        setBrandColor(getResources().getColor(R.color.fastlane_background));
+        setHeaderPresenterSelector(new PresenterSelector()
+        {
+            @Override
+            public Presenter getPresenter(Object o)
+            {
+                return new PictureHeaderItemPresenter();
+            }
+        });
+    }
+
+    private void setupHeaderPictures()
     {
         List<Picture> pictures = PictureList.getPictures();
 
@@ -66,69 +77,36 @@ public class MainFragment extends BrowseFragment
         setAdapter(mRowsAdapter);
     }
 
-    private void prepareBackgroundManager()
-    {
-        mBackgroundManager = BackgroundManager.getInstance(getActivity());
-        mBackgroundManager.attach(getActivity().getWindow());
-        mMetrics = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
-    }
-
-    private void setupUIElements()
-    {
-        setHeadersState(HEADERS_ENABLED);
-        setHeadersTransitionOnBackEnabled(true);
-        setBrandColor(getResources().getColor(R.color.fastlane_background));
-        setHeaderPresenterSelector(new PresenterSelector() {
-            @Override
-            public Presenter getPresenter(Object o) {
-                return new PictureHeaderItemPresenter();
-            }
-        });
-    }
-
     private void setupEventListeners()
     {
-        //setOnItemViewClickedListener(new ItemViewClickedListener());
-        setOnItemViewSelectedListener(new ItemViewSelectedListener());
+        setOnItemViewSelectedListener(new OnItemViewSelectedListener()
+        {
+            @Override
+            public void onItemSelected(Presenter.ViewHolder itemViewHolder, Object item,
+                                       RowPresenter.ViewHolder rowViewHolder, Row row)
+            {
+                HeaderItem headerItem = row.getHeaderItem();
+                if (headerItem instanceof PictureHeaderItem)
+                {
+                    PictureHeaderItem pictureHeaderItem = (PictureHeaderItem) headerItem;
+                    updateBackground(pictureHeaderItem.getPicture().getImageId());
+                }
+            }
+        });
     }
 
     protected void updateBackground(int picId)
     {
         Glide.with(getActivity())
                 .load(picId)
-                .into(new SimpleTarget<GlideDrawable>(mMetrics.widthPixels, mMetrics.heightPixels)
+                .into(new SimpleTarget<GlideDrawable>(metrics.widthPixels, metrics.heightPixels)
                 {
                     @Override
                     public void onResourceReady(GlideDrawable resource,
                                                 GlideAnimation<? super GlideDrawable> glideAnimation)
                     {
-                        mBackgroundManager.setDrawable(resource);
+                        backgroundManager.setDrawable(resource);
                     }
                 });
-    }
-
-    /*private final class ItemViewClickedListener implements OnItemViewClickedListener
-    {
-        @Override
-        public void onItemClicked(Presenter.ViewHolder itemViewHolder, Object item,
-                                  RowPresenter.ViewHolder rowViewHolder, Row row)
-        {
-        }
-    }*/
-
-    private final class ItemViewSelectedListener implements OnItemViewSelectedListener
-    {
-        @Override
-        public void onItemSelected(Presenter.ViewHolder itemViewHolder, Object item,
-                                   RowPresenter.ViewHolder rowViewHolder, Row row)
-        {
-            HeaderItem headerItem = row.getHeaderItem();
-            if (headerItem instanceof PictureHeaderItem)
-            {
-                PictureHeaderItem pictureHeaderItem = (PictureHeaderItem) headerItem;
-                updateBackground(pictureHeaderItem.getPicture().getImageId());
-            }
-        }
     }
 }
